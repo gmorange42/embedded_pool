fc54fd403d37e96dca5b54999bae2c35bac918c9
Guillaume MORANGE <gmorange@bess-f4r4s11.clusters.42paris.fr>
2024-04-15 18:07:04 +0200

    OK

--- ex01/main.c
+++ ex01/main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	DDRB |= (1 << DDB0); // set the bit 0 of port B to 1 (OUTPUT)
-	PORTB |=  (1 << PB0);   // set PB0 to 1 (LED on)
+	DDRB |= (1 << DDB0); // set the bit 0 of Data Direction Register B to 1 (OUTPUT)
+	PORTB |=  (1 << PB0);   // set the bit 0 of PORT B to 1 (LED on)
 	return 0;
 }
