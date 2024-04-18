296351a77b59b64f62938c63f765b4df09508e7a
Guillaume MORANGE <gmorange@bess-f4r4s11.clusters.42paris.fr>
2024-04-18 10:15:14 +0200

    Day02 ok

--- Day02/ex00/main.c
+++ Day02/ex00/main.c
@@ -7,22 +7,30 @@
 #define SWI_BITS(reg, mask) (reg ^= mask);
 #define CHK_BITS(reg, mask) ((reg & mask) == mask)
 
-ISR(INT0_vect)
+volatile uint8_t pressed = 0;
+
+ISR(INT0_vect) // External Interrupt Requeste 0
 {
-	SWI_BITS(PORTB, (1<<PB0));
+	pressed = 1;
 }
 
 int main(void)
 {
 	SET_BITS(DDRB, (1<<PB0)); // Set DDRB bit 0 to 1 (OUTPUT)
-
 	SET_BITS(SREG, (1<<7)); // Set bit 7 in Status Register to 1 to enable Global Interrupt
 	SET_BITS(EICRA, (1<<ISC01)); // Set the Interrupt Sence Controle to Falling
 	SET_BITS(EIMSK, (1<<INT0)); // External Interrupt Request 0 Enable
 
 	while (1)
 	{
-		_delay_ms(100);
+		if (pressed)
+		{
+			CLR_BITS(SREG, (1<<7)); // Set bit 7 in Status Register to 0 to desable Global Interrupt
+			SWI_BITS(PORTB, (1<<PB0));
+			_delay_ms(500);
+			pressed = 0;
+			SET_BITS(SREG, (1<<7)); // Set bit 7 in Status Register to 1 to enable Global Interrupt
+		}
 	}
 
 	return (0);
--- Day02/ex01/main.c
+++ Day02/ex01/main.c
@@ -7,71 +7,65 @@
 #define SWI_BITS(reg, mask) (reg ^= mask);
 #define CHK_BITS(reg, mask) ((reg & mask) == mask)
 
-volatile uint8_t ratio = 0;
-volatile uint8_t up = 1;
+volatile uint8_t ratio = 0; // Use to update OCR1A (represents ignition time as a percentage)
+volatile uint8_t up = 1; // Use to know if ratio should be increased or decreased
 
 void change_ratio(void)
 {
-	if (up)
-	{
-		OCR1A = ++ratio;
-	}
-	else
-	{
-		OCR1A = --ratio;
-	}
+	OCR1A = up ? ++ratio : --ratio; // Update OCR1A
 	if (ratio == 0 || ratio == 100)
 	{
-		SWI_BITS(up, (1<<0));;
+		SWI_BITS(up, (1<<0)); // Switch up
 	}
 }
 
-ISR(TIMER0_OVF_vect)
+ISR(TIMER0_OVF_vect) // Interrupt if Timer0 overflow
 {
 	change_ratio();
 }
 
 int main(void)
 {
-	uint8_t ratio = 1;
-	SET_BITS(DDRB, (1<<1)); // set DDRB1 to OUTPUT
-	SET_BITS(DDRB, (1<<0)); // set DDRB0 to OUTPUT
+	// Set DDRB1 to OUTPUT
+	SET_BITS(DDRB, (1<<1));
 	
-	// Set Comparison Value for Timer1
-	OCR1A = ratio;
+	// Set Status Register to Global Interrupt Enable
+	SET_BITS(SREG, (1<<7));
 
-	// Set Comparison Value for Timer0
+	//TIMER0
+	// Set Comparison Value
 	OCR0A = (F_CPU / 1024 / 200);
 
-	// Set the Input Capture Unit 
-	ICR1 = 100;
-
-	// Set Timer1 top to mode 14
-	SET_BITS(TCCR1A, (1<<WGM11));
-	SET_BITS(TCCR1B, (1<<WGM12) | (1<<WGM13));
-
-	// Set Timer0 top to mode 7
+	// Set top to mode 7
 	SET_BITS(TCCR0A, (1<<WGM00) | (1<<WGM01));
 	SET_BITS(TCCR0B, (1<<WGM02));
 
-	// Set Clock parameter Timer1
-	SET_BITS(TCCR1B, (1<<CS12)); 
-
-	// Set Clock parameter Timer0
+	// Set Clock Source to clki/o/1024 (From prescaler)
 	SET_BITS(TCCR0B, (1<<CS02) | (1<<CS00)); 
 
-	// Set Timer1 output
-	SET_BITS(TCCR1A, (1<<COM1A1));
-
+	// Set Timer/Counter0 Overflow Interrupt Enable
 	SET_BITS(TIMSK0, (1<<TOIE0));
 
-	sei();
+
+	// TIMER1
+	// Set Comparison Value
+	OCR1A = ratio;
+
+	// Set the Input Capture Unit (represent the top of Timer1 in Mode 14) 
+	ICR1 = 100;
+
+	// Set Compare Output Mode (OC1A = 0 if compare is true, OC1A = 1 if BOTTOM);
+	SET_BITS(TCCR1A, (1<<COM1A1));
+
+	// Set top to mode 14
+	SET_BITS(TCCR1A, (1<<WGM11));
+	SET_BITS(TCCR1B, (1<<WGM12) | (1<<WGM13));
+
+	// Set Clock Source to clki/o/1024 (From prescaler)
+	SET_BITS(TCCR1B, (1<<CS12) | (1<<CS10)); 
 
 	while (1)
 	{
 	}
 	return (0);
 }
-
-//Timer0 sert a changer le ratio du rapport cyclique.
-//Timer1 allume et eteind la led suivant la rapport cyclique.
